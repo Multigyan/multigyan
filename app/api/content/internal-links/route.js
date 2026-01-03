1845161cfe50b570aa42ea923ca4f2fa4ebb9337
Vishal Kumar Sharma <vishalkumar.sharma37@gmail.com>
2026-01-03 18:37:09 +0530

    Fix: Internal Link Route

--- app/api/content/internal-links/route.js
+++ app/api/content/internal-links/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import connectDB from '@/@lib/mongodb'
+import connectDB from '@/lib/mongodb'
 import Post from '@/models/Post'
 import { findRelatedPosts, injectInternalLinks } from '@/lib/internal-linking'
 
